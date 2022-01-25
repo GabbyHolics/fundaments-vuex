d0a8571390b6532d6c326aa610c68f47a7219911
GabbyHolics <karenthgabriela@gmail.com>
2022-01-25 09:24:39 -0300

    initial state

---
+++ src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>  Vue Acces </h1>
+    <h2>  Direct Access: {{ $store.state.count }} </h2>
+    <h3> {{ count }} </h3>
+
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+
+    computed: {
+        countComputed(){
+        return console.log('hola')
+    },
+    ...mapState(['count'])
+    }
+}
+</script>
